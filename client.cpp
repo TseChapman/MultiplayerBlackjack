7e2a4c1f3e3bc4e541439d4edad62888a259681c
TseChapman <tch222000@gmail.com>
2023-02-11 14:22:04 -0800

    Implement client side request to server

--- client.cpp
+++ client.cpp
@@ -8,26 +8,298 @@
 #include <arpa/inet.h> // inet_ntoa
 #include <netdb.h> // gethostbyname
 #include <unistd.h> // read, write, close
+#include <unordered_map>
 #include <strings.h> // bzero
 #include <netinet/tcp.h> // SO_REUSEADDR
 #include <sys/uio.h> // writev
-
+#include <sstream>
+#include <vector>
+/*
+#include <windows.h>//different header file in linux
+#include <future>
+*/
 using namespace std;
+/*
+// setTimeOut function
+// Source: https://stackoverflow.com/questions/2485058/equivalent-to-window-settimeout-for-c
+template <typename... ParamTypes>
+void setTimeOut(int milliseconds,std::function<void(ParamTypes...)> func,ParamTypes... parames)
+{
+  std::async(std::launch::async,[=]()
+  {
+      Sleep(milliseconds);
+      func(parames...);
+  });
+};
+*/
+void printDebugStatement(bool isDebug, string statement) {
+  if (isDebug) {
+    cout << statement << endl;
+  }
+}
+
+string parseResponseInfo(int socket) {
+  string responseLine = ""; // output string
+  char prev = 0;
+  // Check each character of the response line
+  while (true) {
+    char curr = 0;
+    read(socket, &curr, 1);
+    if (curr == '\n' || curr == '\r') {
+      if (curr == '\n' && prev == '\r') { break; }
+    }
+    else {
+      responseLine += curr;
+    }
+    prev = curr;
+  }
+  return responseLine; // return the response line
+}
+
+struct ActionResponse {
+  int status;
+  string player_id;
+  string action;
+  vector<string> information;
+};
+
+string formatRequest(string inputAction, const int& ack, const string& player_id) {
+  // Get strings that is splitted by space
+  vector<string> inputs;
+  stringstream ss(inputAction);
+  string str;
+
+  while (getline(ss, str, ' ')) {
+    inputs.push_back(str);
+  }
+
+  // Map of commands
+  unordered_map<string, int> command_map = {
+    {"EXIT", 1},
+    {"CREATE_GAME", 2},
+    {"EXIT_GAME", 3},
+    {"LIST", 4},
+    {"CREATE_USER", 5},
+    {"JOIN", 6}
+  };
+
+  int command;
+  try {
+    command = command_map[inputs.at(0)];
+  } catch (...) {
+    cerr << "Command not Found" << endl;
+    return "";
+  }
+
+  // Format request
+  string res = "";
+  switch (command) {
+    case 1:
+      // E.g. "EXIT\r\nAck:ack\r\nPlayer ID:player_id\r\n\r\n"
+      res = inputs.at(0) + "\r\n" + "Ack:" + to_string(ack) + "\r\n" + "Player ID:" + player_id + "\r\n\r\n";
+      break;
+    case 2:
+      try {
+        // E.g. "CREATE_GAME lobby_name\r\nAck:ack\r\nPlayer ID:player_id\r\n\r\n"
+        res = inputs.at(0) + " " + inputs.at(1) + "\r\n" + "Ack:" + to_string(ack) + "\r\n" + "Player ID:" + player_id + "\r\n\r\n";
+        // E.g. Response: "OK\r\nAck:ack\r\nPlayer ID:player_id\r\nAction:CREAT_GAME\r\nLobby ID:lobby_id\r\n\r\n"
+      } catch (...) {
+        cerr << "Invalid number of parameters for action: " + inputs.at(0) << endl;
+      }
+      break;
+    case 3:
+      try {
+        // E.g. "EXIT_GAME lobby_id\r\nAck:ack\r\nPlayer ID:player_id\r\n\r\n"
+        res = inputs.at(0) + " " + inputs.at(1) + "\r\nAck:" + to_string(ack) + "\r\nPlayer ID:" + player_id + "\r\n\r\n";
+        // E.g. Response: "OK\r\nAck:ack\r\nPlayer ID:player_id\r\nAction:EXIT_GAME\r\nLobby ID:lobby_id\r\n\r\n"
+      } catch (...) {
+        cerr << "Invalid number of parameters for action: " + inputs.at(0) << endl;
+      }
+      break;
+    case 4:
+      // E.g. "LIST\r\nAck:ack\r\nPlayer ID:player_id\r\n\r\n"
+      res = inputs.at(0) + "\r\nAck:" + to_string(ack) + "\r\nPlayer ID:" + player_id + "\r\n\r\n";
+      // E.g. Response: "OK\r\nAck:ack\r\nPlayer ID:player_id\r\nAction:LIST\r\nNumber of Lobby:x\r\nLobby ID:lobby_id\r\nLobby Name:lobby_name\r\n\r\n"
+      break;
+    case 5:
+    case 6:
+      // TODO
+      // E.g. "JOIN lobby_id\r\nAck:ack\r\nPlayer ID:player_id\r\n\r\n"
+      // E.g. Response: "OK\r\nAck:ack\r\nPlayer ID:player_id\r\nAction:LIST\r\nLobby ID:lobby_id\r\nLobby Name:lobby_name\r\n\r\n"
+  }
+  return res;
+}
+
+ActionResponse performAction(const addrinfo* servInfo, const string inputAction, int& ack, const string player_id, int isDebug) {
+  ActionResponse res;
+  // Format an action
+  string request = formatRequest(inputAction, ack, player_id);
+
+  if (request == "") {
+    cout << "Invalid action" << endl;
+    res.status = 0;
+    return res;
+  }
+  try {
+    // Create client socket
+    printDebugStatement(isDebug, "Connecting to server with Player ID: " + player_id);
+    int clientSd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
+
+    // Connect to server and check connection status
+    int status = connect(clientSd, servInfo->ai_addr, servInfo->ai_addrlen);
+    if (status < 0) {
+      cerr << "Failed to connect to the server" << endl;
+      close(clientSd);
+      res.status = 0;
+      return res;
+    }
+
+    // Write the action to server
+    printDebugStatement(isDebug, "Request:\n" + request);
+    int writeRes = write(clientSd, request.c_str(), strlen(request.c_str()));
+    if (writeRes <= 0) {
+      cerr << "Unable to send the request" << endl;
+      res.status = 0;
+      return res;
+    }
+
+    // Read response from server
+    // Get Server response
+    printDebugStatement(isDebug, "Request Response: ");
+    try {
+      while (true) {
+        string responseHeader = parseResponseInfo(clientSd);
+        if (responseHeader == "") {
+          break;
+        }
+        printDebugStatement(isDebug, "Response Header:\n" + responseHeader);
+        if (responseHeader.substr(0,2) == "OK") {
+          res.status = 1;
+        }
+        else if (responseHeader.substr(0,2) == "NO") {
+          res.status = 0;
+        }
+        else if (responseHeader.substr(0, 5) == "Ack:") {
+          int ack_recv = atoi(responseHeader.substr(5, responseHeader.length()).c_str());
+          if (ack_recv == ack) {
+            ack++;
+          }
+        }
+        else if (responseHeader.substr(0, 10) == "Player ID:") {
+          string player_id_recv = responseHeader.substr(10, responseHeader.length()).c_str();
+          res.player_id = player_id_recv;
+        }
+        else if (responseHeader.substr(0, 7) == "Action:") {
+          res.action = responseHeader.substr(7, responseHeader.length()).c_str();
+        }
+        else {
+          res.information.push_back(responseHeader);
+        }
+      }
+    } catch (...) {
+      cerr << "Error happened in reading server response to perform request." << endl;
+      close(clientSd);
+      res.status = 0;
+      return res;
+    }
+    close(clientSd);
+  } catch (...) {
+      res.status = 0;
+      return res;
+  }
+  return res;
+}
+
+
+struct GetUsernameReturn {
+  string player_id;
+  string username;
+};
+
+GetUsernameReturn getValidUsername(const addrinfo* servInfo, int& ack, int isDebug) {
+  GetUsernameReturn res;
+  bool isUsernameSet = false;
+  try {
+    while (!isUsernameSet) {
+      // Ask player for username
+      string username = "";
+      cout << "Please type your username (no space, no symbol), press ENTER to submit: ";
+      getline(cin, username);
+
+      // Create client socket
+      cout << "Try Joining server with username: " + username << endl;
+      int clientSd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
+
+      // Connect to server and check connection status
+      int status = connect(clientSd, servInfo->ai_addr, servInfo->ai_addrlen);
+      if (status < 0) {
+        cerr << "Failed to connect to the server" << endl;
+        close(clientSd);
+        continue;
+      }
+
+      // Make a create username request to server
+      string request = string("CREATE_USER " + string(username) + "\r\n" + "Ack:" + to_string(ack) + "\r\n\r\n");
+      // E.g. Response "OK\r\nAck:ack\r\nPlayer ID:player_id\r\n\r\n"
+      printDebugStatement(isDebug, "Request:\n" + request);
+
+      int writeRes = write(clientSd, request.c_str(), strlen(request.c_str()));
+      if (writeRes <= 0) {
+        cerr << "Unable to send the request" << endl;
+        close(clientSd);
+        continue;
+      }
+
+      // TODO: Set timer
+
+      // Get Server response
+      printDebugStatement(isDebug, "Request Response: ");
+      try {
+        while (true) {
+          string responseHeader = parseResponseInfo(clientSd);
+          if (responseHeader == "") {
+            break;
+          }
+          printDebugStatement(isDebug, "Response Header:\n" + responseHeader);
+          if (responseHeader.substr(0,2) == "OK") {
+            res.username = username;
+            isUsernameSet = true;
+          }
+          if (responseHeader.substr(0, 5) == "Ack:") {
+            int ack_recv = atoi(responseHeader.substr(5, responseHeader.length()).c_str());
+            if (ack_recv == ack) {
+              ack++;
+            }
+          }
+          if (responseHeader.substr(0, 10) == "Player ID:") {
+            string player_id_recv = responseHeader.substr(10, responseHeader.length()).c_str();
+            res.player_id = player_id_recv;
+          }
+        }
+      } catch (...) {
+        cerr << "Error happened in reading server response to create username." << endl;
+        close(clientSd);
+        continue;
+      }
+      close(clientSd);
+    }
+  } catch (...) {
+    cerr << "Error happened in create username." << endl;
+  }
+  printDebugStatement(isDebug, "Username: " + res.username + ", Player ID: " + res.player_id);
+  return res;
+}
 
 int main(int argc, char* argv[]) {
   // Validate parameters
-  if (argc != 6) { return -1; }
+  if (argc != 4) { return -1; }
   char* serverName;
   char* serverPort;
-  char* username;
-  int nbufs;
-  int bufsize;
+  int isDebug;
   try {
     serverName = argv[1];
     serverPort = argv[2];
-    username = argv[3];
-    nbufs = stoi(argv[4]);
-    bufsize = stoi(argv[5]);
+    isDebug = stoi(argv[3]);
   } catch (...) {
     cerr << "Error happened getting parameter" << endl;
     return -1;
@@ -38,101 +310,42 @@ int main(int argc, char* argv[]) {
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  getaddrinfo(serverName, serverPort, &hints, &servInfo);
 
-  try {
-    // Make a socket, bind it, and listen on it
-    int clientSd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
-
-    int status = connect(clientSd, servInfo->ai_addr, servInfo->ai_addrlen);
-    if (status < 0) {
-      cerr << "Failed to connect to the server" << endl;
-      close(clientSd);
-      return -1;
-    }
-
-    char databuf[nbufs][bufsize]; // where nbufs * bufsize = 1500
-
-    // Save start time
-    struct timeval start;
-    gettimeofday(&start, NULL);
-
-    // write username to server
-    // While loop
-    // read server return status
-    // If return status is false, ask for new username and write to server again
-    // if return status is true, username is set, break the loop.
-
-    // While loop
-    // Get user input command
-    // write action code to server
-    // read server action code status
-    /*
-    use cases:
-    1.
-    2.
-    3.
-    */
-    // Read server output
-    // Display on screen
-
-
-    // Handle write to server by type
-    /*
-    if (type == 1) {
-      for (int i = 0; i < iterations; i++) {
-        for (int j = 0; j < nbufs; j++) {
-          int x = write(clientSd, databuf[j], bufsize);
-          if (x < 0) {
-            cerr << "Problem with multiple writes" << endl;
-          }
-        }
-      }
-    }
-    else if (type == 2) {
-    for (int i = 0; i < iterations; i++) {
-        struct iovec vector[nbufs];
-        for (int j = 0; j < nbufs; j++) {
-          vector[j].iov_base = databuf[j];
-          vector[j].iov_len = bufsize;
-        }
-        writev(clientSd, vector, nbufs);
-      }
-    }
-    else if (type == 3) {
-      for (int i = 0; i < iterations; i++) {
-        write(clientSd, databuf, nbufs * bufsize);
-      }
-    }
-    */
-    // Save transmission time
-    struct timeval lap;
-    gettimeofday(&lap, NULL);
-    suseconds_t transmission_time = (
-      (lap.tv_sec * 1000000 + lap.tv_usec) -
-      (start.tv_sec * 1000000 + start.tv_usec)
-    );
-
-    // Use read to receive a response from the server
-    /*
-    int count = 0;
-    read(clientSd, &count, sizeof(count));
-    */
-    // Save round trip time
-    struct timeval stop;
-    gettimeofday(&stop, NULL);
-    suseconds_t round_trip_time = (
-      (stop.tv_sec * 1000000 + stop.tv_usec) -
-      (start.tv_sec * 1000000 + start.tv_usec)
-    );
-
-    // Display output
-    cout << "Test 1: data-transmission time = " << transmission_time << " usec, round-trip time = " << round_trip_time
-  << " usec, #reads = " << count << endl;
-    close(clientSd);
-  } catch (...) {
-    cerr << "Error happened when connecting with server" << endl;
+  // Check server address status
+  int serverAddrInfoStatus = getaddrinfo(serverName, serverPort, &hints, &servInfo);
+  if (serverAddrInfoStatus != 0) {
+    cerr << "Unable to connect" << endl;
     return -1;
   }
 
+  int ack = 0; // ACK
+  bool isJoined = false;
+
+  // Setup Player account
+  GetUsernameReturn res = getValidUsername(servInfo, ack, isDebug);
+  string player_id = res.player_id;
+  string username = res.username;
+
+  // Start the game
+  // Start by loading a list of lobby
+  try {
+
+    while (isJoined) {
+      // Read Player input
+      string input;
+      cout << "Type action (Type 'HELP' for list of command): ";
+      getline(cin, input);
+
+      // Perform action based on input
+      ActionResponse res = performAction(servInfo, input, ack, player_id, isDebug);
+      if (res.status == 0 || input.substr(0,4) == "HELP") {
+        // Print a list of command
+        continue;
+      }
+
+      // Process response
+    }
+  } catch (...) {
+
+  }
 }
--- server.cpp
+++ server.cpp
@@ -1,22 +1,227 @@
-#include <iostream>
+#include <arpa/inet.h> // inet_ntoa
 #include <cstring>
+#include <errno.h>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <netinet/in.h> // htonl, htons, inet_ntoa
+#include <netinet/tcp.h> // SO_REUSEADDR
+#include <netdb.h> // gethostbyname
+#include <unistd.h> // read, write, close
+#include <unordered_map>
 #include <string>
+#include <strings.h> // bzero
 #include <sys/types.h> // socket, bind
 #include <sys/socket.h> // socket, bind, listen, inet_ntoa
 #include <sys/time.h>
-#include <netinet/in.h> // htonl, htons, inet_ntoa
-#include <arpa/inet.h> // inet_ntoa
-#include <netdb.h> // gethostbyname
-#include <unistd.h> // read, write, close
-#include <strings.h> // bzero
-#include <netinet/tcp.h> // SO_REUSEADDR
 #include <sys/uio.h> // writev
+#include <vector>
 
 using namespace std;
 
-struct thread_data {
-  int sd;
+extern int errno;
+
+const int MAX_CON = 10;
+
+struct Player {
+  string player_id;
+  string username;
+  int status; // 0 for not active, 1 for active
+  string lobby_id; // empty if not in lobby
 };
+
+unordered_map<string, Player> players;
+pthread_mutex_t players_mutex;
+
+void printDebugStatement(bool isDebug, string statement) {
+  if (isDebug) {
+    cout << statement << endl;
+  }
+}
+
+struct thread_data {
+  int thread_id;
+  int sd;
+  int isDebug;
+};
+
+string parseRequestInfo(int sd) {
+  // Parse request header from client socket
+  string requestLine = "";
+  char prev = 0;
+  while (true) {
+    char curr = 0;
+    read(sd, &curr, 1);
+    if (curr == '\n' || curr == '\r') {
+      if (curr == '\n' && prev == '\r') { break; }
+    }
+    else {
+      requestLine += curr;
+    }
+    prev = curr;
+  }
+  return requestLine;
+}
+
+struct requestData {
+  string action;
+  int ack;
+  string player_id;
+  vector<string> information;
+};
+
+string processRequestData(const requestData& request) {
+  // Determine the action
+  // Map of commands
+  unordered_map<string, int> command_map = {
+    {"EXIT", 1},
+    {"CREATE_GAME", 2},
+    {"EXIT_GAME", 3},
+    {"LIST", 4},
+    {"CREATE_USER", 5},
+    {"JOIN", 6}
+  };
+
+  int command;
+  try {
+    command = command_map[request.action];
+  } catch (...) {
+    cerr << "Command not Found" << endl;
+    // TODO: Finalize Invalid request
+    response = "NO\r\nAck:" + to_string(request.ack) +"\r\nPlayer ID:" + request.player_id + "\r\n\r\n";
+  }
+
+  string response = "";
+  switch (command) {
+    case 1:
+      // Exit the game
+      pthread_mutex_lock(&players_mutex);
+      // Step 1: Exit lobby if any
+      try {
+        if (!players[request.player_id].lobby_id.empty()) {
+          // TODO: Exit lobby
+        }
+      } catch (...) {
+
+      }
+      // Step 2: deactive username
+      try {
+        players.erase(request.player_id);
+      } catch (...) {
+
+      }
+      pthread_mutex_unlock(&players_mutex);
+    case 2:
+      // Step 1: Search list of lobby
+      // Step 2: Check if non of the lobby has the same name
+      // Step 3: Add the lobby name to the list
+      // Step 4: Generate lobby id
+      // Step 5: Write to client
+    case 3:
+      // Step 1: Search for lobby with lobby_id
+      // Step 2: Check if player_id in lobby
+      // Step 3: Remove player from lobby
+      // Step 4: Reset player's lobby id
+    case 4:
+      // Step 1: Get a list of lobby
+      // Step 2: Format response
+    case 5:
+      // Step 1: Get the list of player
+      // Step 2: Check if the username is taken
+      // Step 3: Generate player with username and new unique player id
+      // Step 4: Add the player to the list
+      // Step 5: Format response
+  }
+  return response;
+}
+
+void *processRequest(void *arg) {
+  struct thread_data *data = (struct thread_data*)arg; // data object
+  // Process the request
+  string response = "";
+  requestData request;
+  int iterations = 0;
+  while (true) {
+    // Retreive the request line
+    string requestLine = parseRequestInfo(data->sd);
+    if (requestLine == "") {
+      cerr << "Incorrect request line format" << endl;
+      break;
+    }
+
+    printDebugStatement(isDebug, "Response Header:\n" + requestLine);
+    // Get the request action
+    if (iterations == 0) {
+      // Split the action by space if any
+      vector<string> inputs;
+      stringstream ss(requestLine);
+      string str;
+
+      while (getline(ss, str, ' ')) {
+        inputs.push_back(str);
+      }
+
+      // Determine the action
+      // Map of commands
+      unordered_map<string, int> command_map = {
+        {"EXIT", 1},
+        {"CREATE_GAME", 2},
+        {"EXIT_GAME", 3},
+        {"LIST", 4},
+        {"CREATE_USER", 5},
+        {"JOIN", 6}
+      };
+
+      int command;
+      try {
+        command = command_map[inputs.at(0)];
+      } catch (...) {
+        cerr << "Command not Found" << endl;
+        // TODO: Finalize Invalid request
+        response = "NO\r\n"
+      }
+
+      switch (command) {
+        case 2:
+        case 3:
+        case 5:
+          request.information.push_back(inputs.at(1));
+        default:
+          request.action = inputs.at(0);
+      }
+    }
+
+    // Get Ack
+    else if (iterations == 1) {
+        if (requestLine.substr(0, 5) == "Ack:") {
+        int ack_recv = atoi(requestLine.substr(5, requestLine.length()).c_str());
+        request.ack = ack_recv;
+      }
+    }
+
+    // Get Player ID
+    else if (iterations == 2) {
+      if (requestLine.substr(0, 10) == "Player ID:") {
+        string player_id_recv = requestLine.substr(10, requestLine.length()).c_str();
+        res.player_id = player_id_recv;
+      }
+    }
+    else {
+      res.information.push_back(requestLine);
+    }
+    iterations++;
+    if (iterations > 100) { break; }
+  }
+
+  // Get the file content based on the requested file
+  response = processRequestData(res);
+  printDebugStatement(data->isDebug)
+  cout << "Complete Response: " + response << endl;
+
+  // Write the file and request status back to the client
+  write(data->sd, &response[0], response.size());
+  close(data->sd);
+}
 
 static void * serverThreadFunc(void *arg) {
   // Define server function variables
@@ -64,49 +269,84 @@ static void * serverThreadFunc(void *arg) {
   close(data->sd);
 }
 
-int main (int argc, char* argv[]) {
-  // Validate parameters
-  if (argc != 2) { return -1; }
-  char* port;
-  try {
-    port = argv[1];
-  } catch (...) {
-    cerr << "Error happened getting parameter" << endl;
+int main(int argc, char* argv[]) {
+  // Must input 1 and only 1 parameter
+  if (argc != 3) {
+    cerr << "Invalid number of argument. The program takes 2 arguments: server port number, 0 or 1 for doing debug or not." << endl;
     return -1;
   }
 
+  // Get the port from argument
+  char* port;
+  int isDebug;
+  try {
+    port = argv[1];
+    isDebug = stoi(argv[2]);
+  } catch (...) {
+    cerr << "Error happens when parsing argument." << endl;
+    return -1;
+  }
+
+  cout << "Get address information" << endl;
+
   // Get address information
-  struct addrinfo hints, *res;
+  struct addrinfo hints, *servInfo;
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
-  getaddrinfo(NULL, port, &hints, &res);
 
+  int addressInfoStatus = getaddrinfo(NULL, port, &hints, &servInfo);
+  if (addressInfoStatus != 0) {
+    cerr << "Unable to connect" << endl;
+    return -1;
+  }
+
+  cout << "Starting server" << endl;
+
+  int serverSd;
   try {
     // Create Server
-    int serverSd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    serverSd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
     const int yes = 1;
-    const int maxNumClient = 100;
     setsockopt(serverSd, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes));
-    bind(serverSd, res->ai_addr, res->ai_addrlen);
-    listen(serverSd, maxNumClient);
+    bind(serverSd, servInfo->ai_addr, servInfo->ai_addrlen);
+    listen(serverSd, MAX_CON);
+  } catch (...) {
+    cerr << "Error happens in creating server." << endl;
+    return -1;
+  }
 
-    // Accept an incomming connection
-    struct sockaddr_storage clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr);
+  cout << "Start accepting client connection" << endl;
+
+  try {
+    int iterations = 1;
     while (1) {
+      // Accept an incomming connection
+      struct sockaddr_storage clientAddr;
+      socklen_t clientAddrSize = sizeof(clientAddr);
       // Accept connection
       int newSd = accept(serverSd, (struct sockaddr *)&clientAddr, &clientAddrSize);
 
-      // Start new thread and perform function serverThreadFunc
+      if (newSd == -1) {
+        cerr << "Unable to connect to client." << endl;
+        continue;
+      }
+
+      // Start new thread and perform function processRequest
       pthread_t newThread;
       struct thread_data *data = new thread_data;
+      data->thread_id = iterations;
       data->sd = newSd;
-      int clientAction = pthread_create(&newThread, NULL, serverThreadFunc, (void*)data);
+      data->isDebug = isDebug;
+      iterations++;
+
+      cout << "Creating new thread with thread id: " + to_string(iterations) << endl;
+
+      int threadRes = pthread_create(&newThread, NULL, processRequest, (void*)data);
     }
   } catch (...) {
-    cerr << "Error happened in creating server and accepting connections" << endl;
+    cerr << "Error happens in accepting client connection and creating threads." << endl;
     return -1;
   }
 }
